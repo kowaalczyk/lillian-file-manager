21048e78507777f21fd7168fd15fbc659995da97
Tomasz Miśków <miskow.tomasz@gmail.com>
2018-05-08 00:15:45 +0200

    add renderer communication

---
+++ renderer.js
@@ -0,0 +1,9 @@
+const {ipcRenderer, remote} = require('electron');
+const main = remote.require("./main.js");
+
+document.addEventListener("DOMContentLoaded", function () {
+    ipcRenderer.send('request', "/usr/src/minix");
+    ipcRenderer.send('request', {name: "Tomek", age: 20, dogs: ["Burek", "Reksio"]});
+    ipcRenderer.send('request', 1, 2, 3, 4);
+    ipcRenderer.send('request', ["usr", "src", "minix"]);
+});
